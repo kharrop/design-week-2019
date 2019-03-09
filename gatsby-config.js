df99052300320d7452d8ef4ed7abfba1cb104ddb
Kelly Harrop <kelly_harrop@intuit.com>
2019-03-09 14:38:55 -0600

    wordpress integration

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
     title: `Design Week 2019`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    description: `Event Details for Design Week 2019`,
+    author: `PCG Design Squad`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,6 +25,24 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        baseUrl: "kellydesigntest.wordpress.com",
+        protocol: "https",
+        hostingWPCOM: true,
+        useACF: false,
+        auth: {
+          wpcom_app_clientSecret:
+            "tFeSCu64s6iJbUzRqaKFRp7bO0FhxKM3UxTIhLi8AoiYQDSgbWQnggPQnqC6UN5v",
+          wpcom_app_clientId: "64972",
+          wpcom_user: "kn.harrop@gmail.com",
+          wpcom_pass: "nE7!q9ShQ$%%C6P^eEDdS!34X&SHbs!6j%2#k",
+        },
+
+        verboseOutput: false,
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,41 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
+export const pageQuery = graphql`
+  query {
+    allWordpressPost(sort: { fields: [date] }) {
+      edges {
+        node {
+          title
+          excerpt
+          slug
+        }
+      }
+    }
+  }
+`
+
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi, people!</h1>
+    <h1>Hi, people!!</h1>
     <p>Welcome to Design Week 2019.</p>
     <p>July 16-18, 2019</p>
+    <div>
+      <p>We're a very cool website you should return to often.</p>
+    </div>
+    {data.allWordpressPost.edges.map(({ node }) => (
+      <div>
+        <Link to={node.slug} css={{ textDecoration: `none` }}>
+          <p>{node.title}</p>
+        </Link>
+        <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
+      </div>
+    ))}
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
