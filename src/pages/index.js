283df209e1cdaff6d71c353ebce451571be17ada
Kelly Harrop <kelly_harrop@intuit.com>
2019-03-08 21:07:44 -0600

    test

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
+    <h1>Hi, people!</h1>
     <p>Welcome to Design Week 2019.</p>
     <p>July 16-18, 2019</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
